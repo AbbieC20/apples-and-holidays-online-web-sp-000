87fa72cf41760b8ab30037a6787ee67b72d07207
remote-torrent-6747 <abbyc@sky.com>
2020-12-28 20:38:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,9 +30,9 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season|
+  holiday_hash.each do |season, holiday|
     puts "#{season}:"
-      supplies.each do |holiday, supplies|
+      holiday.each do |holiday, supplies|
         puts "#{holiday}: #{supplies}"
       end
   end
