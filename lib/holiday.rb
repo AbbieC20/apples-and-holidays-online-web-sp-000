53a0381f6d6b052877a3828ae07f08a795bf08c4
remote-torrent-6747 <abbyc@sky.com>
2020-12-28 21:13:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
       holidays.each do |holiday, supplies|
-        puts holiday
         holiday_words = holiday.to_s.split('_')
         holiday_words.each_with_index do |word, index|
           holiday_words[index] = word.capitalize
