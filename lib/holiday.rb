967dc61ae23cc3b87a199248bb1b00645bf8d49a
remote-torrent-6747 <abbyc@sky.com>
2020-12-28 20:03:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  
+  holiday_hash.values.flatten
+  return holiday_hash[0,1,2]
 
 end
 
