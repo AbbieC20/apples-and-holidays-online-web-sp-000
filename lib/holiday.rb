68eaeab6e8f0b97b4a56e388fade0c3d07d9c978
remote-torrent-6747 <abbyc@sky.com>
2020-12-28 20:02:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     return holiday_hash
 end
 
+def all_winter_holiday_supplies(holiday_hash)
+  # return an array of all of the supplies that are used in the winter season
 
+end
 
 # given that holiday_hash looks like this:
 # {
